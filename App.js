b56137238a00c2edeacf8d579e116f062b8fe09c
U Sai Rajesh <srajesh636@gmail.com>
2020-03-24 12:47:08 +0530

    chore(app) : adds button in deck

--- App.js
+++ App.js
@@ -1,68 +1,63 @@
-import React from 'react';
-import {SafeAreaView, Text, ScrollView} from 'react-native';
-import {Deck} from './components/Deck';
-import {Card, Button} from 'react-native-elements';
+import React from "react";
+import { SafeAreaView, Text, ScrollView } from "react-native";
+import { Deck } from "./components/Deck";
+import { Card, Button } from "react-native-elements";
 
 const DATA = [
   {
     id: 1,
-    text: 'Card #1',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg',
+    text: "Card #1",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg"
   },
   {
     id: 2,
-    text: 'Card #2',
-    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg',
+    text: "Card #2",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg"
   },
   {
     id: 3,
-    text: 'Card #3',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg',
+    text: "Card #3",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg"
   },
   {
     id: 4,
-    text: 'Card #4',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg',
+    text: "Card #4",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg"
   },
   {
     id: 5,
-    text: 'Card #5',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg',
+    text: "Card #5",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg"
   },
   {
     id: 6,
-    text: 'Card #6',
-    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg',
+    text: "Card #6",
+    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg"
   },
   {
     id: 7,
-    text: 'Card #7',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg',
+    text: "Card #7",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg"
   },
   {
     id: 8,
-    text: 'Card #8',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg',
-  },
+    text: "Card #8",
+    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg"
+  }
 ];
 const App = () => {
   const renderCard = item => {
     return (
-      <Card title={item.text} image={{uri: item.uri}}>
-        <Text style={{padding: 10}}>Sample Text</Text>
+      <Card title={item.text} image={{ uri: item.uri }}>
+        <Text style={{ padding: 10 }}>Sample Text</Text>
+        <Button title="View" backgroundColor="#03A9F4" />
       </Card>
     );
   };
   return (
     <SafeAreaView>
       <ScrollView>
-        <Deck
-          data={DATA}
-          renderCard={renderCard}
-          // renderNoMoreCard={}
-          // onSwipeLeft={}
-          // onSwipeRight={}
-        />
+        <Deck data={DATA} renderCard={renderCard} />
       </ScrollView>
     </SafeAreaView>
   );
